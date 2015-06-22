54d6fb7ee7e985a6007bad3b191a03ab7f6e5de3
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2015-06-22 16:15:47 +0100

    geant4: New version 9.6.4
    Import formula from old SuperNEMO tap, keeping version in line with
    Cadfael 1.1.1. Remove options for visualization until basic multi
    platform tests are complete.

---
+++ geant4.rb
@@ -0,0 +1,31 @@
+class Geant4 < Formula
+  homepage "http://geant4.cern.ch"
+  url "http://geant4.cern.ch/support/source/geant4.9.6.p04.tar.gz"
+  version "9.6.4"
+  sha256 "997220a5386a43ac8f533fc7d5a8360aa1fd6338244d17deeaa583fb3a0f39fd"
+
+  depends_on "cmake" => :build
+  depends_on "clhep"
+  depends_on "xerces-c" => :recommended
+
+  def install
+    mkdir 'geant4-build' do
+      args = std_cmake_args
+      args << "-DCMAKE_INSTALL_LIBDIR=lib"
+      args << "-DCMAKE_BUILD_WITH_INSTALL_RPATH=ON"
+      args << "-DGEANT4_INSTALL_DATA=ON"
+      args << "-DGEANT4_USE_SYSTEM_CLHEP=ON"
+      args << "-DGEANT4_USE_SYSTEM_EXPAT=ON"
+      #args << "-DGEANT4_USE_SYSTEM_ZLIB=ON"
+
+      args << "-DGEANT4_USE_GDML=ON" if build.with? "xerces-c"
+
+      system "cmake", "../", *args
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "false"
+  end
+end
