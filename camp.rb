87d80f42c2f70b5af2758c51bc88ba59f12689bf
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2015-06-22 16:00:26 +0100

    camp: new version 0.8.0(git)
    Import Camp formula from old SuperNEMO hombrew tap.

---
+++ camp.rb
@@ -0,0 +1,34 @@
+require 'formula'
+
+class Camp < Formula
+  homepage "https://github.com/tegesoft/camp"
+  url "https://github.com/drbenmorgan/camp.git", :revision => "7564e57f7b406d1021290cf2260334d57d8df255"
+  version "0.8.0"
+
+  option :cxx11
+
+  depends_on "cmake" => :build
+
+  # This appears to be a more robust way of defining options so that the
+  # dependency is always installed
+  option "with-doxygen", "Build with doxygen documentation"
+  depends_on "doxygen" => [:optional, :build] 
+
+  if build.cxx11?
+    depends_on "boost" => "c++11"
+  else
+    depends_on "boost"
+  end
+
+  def install
+    ENV.cxx11 if build.cxx11?
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    system "make", "doc" if build.with? "doxygen"
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
