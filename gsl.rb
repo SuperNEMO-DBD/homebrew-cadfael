f176ab394a355152058c35e542e85e529051bad7
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2015-11-09 17:18:58 +0000

    gsl: Provide CMake support file resource
    Add GSLCMakeSupport repo as a resource to the project. This supplies
    CMake "module" support files for find_package.
    
    Stage, build and install resource as part of install of GSL.
    
    Add a test block to create a mini-project that uses these files to
    configure build and run a simple GSL program.

--- gsl.rb
+++ gsl.rb
@@ -8,6 +8,11 @@ class Gsl < Formula
 
   option :universal
 
+  depends_on "cmake" => :build
+  resource "GSLCMakeSupport" do
+    url "https://github.com/SuperNEMO-DBD/GSLCMakeSupport.git", :using => :git
+  end
+
   def install
     ENV.universal_binary if build.universal?
 
@@ -15,5 +20,36 @@ class Gsl < Formula
                           "--prefix=#{prefix}"
     system "make" # A GNU tool which doesn't support just make install! Shameful!
     system "make install"
+
+    # Install cmake support files
+    resources.each do |r|
+      r.stage do
+        gsl_support_args = std_cmake_args
+        gsl_support_args << "-Dgsl_VERSION=#{version}"
+        system "cmake", *gsl_support_args
+        system "make install"
+      end
+    end
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<-EOS.undent
+    project(testGSL C)
+    find_package(GSL REQUIRED NO_MODULE)
+    add_executable(testGSL testGSL.c)
+    target_include_directories(testGSL PRIVATE ${GSL_INCLUDE_DIRS})
+    target_link_libraries(testGSL gsl)
+    EOS
+
+    (testpath/"testGSL.c").write <<-EOS.undent
+    #include <gsl/gsl_version.h>
+    int main(int argc, char** argv) {
+    return 0;
+    }
+    EOS
+
+    system "cmake", *std_cmake_args
+    system "make"
+    system "./testGSL"
   end
 end
