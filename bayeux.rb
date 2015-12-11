711bf970479707c0da346821db88579f0b7b17ef
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2015-12-11 16:39:49 +0000

    bayeux: Add C++11 option
    Micropatch datatools kernel for single line error that tries to
    set a char* with a single char value.

--- bayeux.rb
+++ bayeux.rb
@@ -6,16 +6,31 @@ class Bayeux < Formula
   sha256 "8a1db5cc6d032a034e79560248328c1bc45b5fda700a47e6fa3bcd1096fa2909"
   revision 1
 
+  option :cxx11
+
   depends_on "cmake" => :build
-  depends_on "boost"
-  depends_on "camp"
+  
   depends_on "doxygen"
   depends_on "gsl"
   depends_on "readline"
-  depends_on "root5"
-  depends_on "geant4"
+
+  if build.cxx11?
+    depends_on "boost" => "c++11"
+    depends_on "camp" => "c++11"
+    depends_on "root5" => "c++11"
+    depends_on "geant4" => "c++11"
+  else
+    depends_on "boost"
+    depends_on "camp"
+    depends_on "root5"
+    depends_on "geant4"
+  end
 
   def install
+    # Micro patch to correct setting of argv with const char* instead of char
+    inreplace "source/bxdatatools/src/kernel.cc", "'\\0'", "\"\\0\""
+
+    ENV.cxx11 if build.cxx11?
     mkdir "bayeux.build" do
       bx_cmake_args = std_cmake_args
       bx_cmake_args << "-DCMAKE_INSTALL_LIBDIR=lib"
