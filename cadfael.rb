9e6796364f98a1effa432e73eda229c5e17486fc
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2015-12-15 14:36:39 +0000

    cadfael: New version 2015.12
    - Install gcc49, binutils on Linux
    - Provide c++11 option to enable C++11 compatibility across packages
    - Migrate to new date-based versioning scheme.

--- cadfael.rb
+++ cadfael.rb
@@ -2,22 +2,41 @@ require 'formula'
 
 class Cadfael < Formula
   url File.dirname(__FILE__), :using => :git
-  version "1.1.1"
+  version "2015.12"
 
   keg_only "Requirement and version tracking formula only"
 
-  # Basic toolset
+  option :cxx11
+
+  # Basic toolset *should* be built before anything else
+  # If it isn't, need to split this off into separate
+  # "toolchain formulae"
+  if OS.linux?
+    depends_on "binutils" => ["with-default-names"]
+    depends_on "gcc49"
+  end
+
   depends_on "pkg-config"
   depends_on "python"
-
-  depends_on "boost"
-  depends_on "camp"
-  depends_on "clhep"
   depends_on "doxygen"
-  depends_on "geant4"
   depends_on "gsl"
-  depends_on "root5"
-  depends_on "xerces-c"
+
+  if build.cxx11?
+    depends_on "boost" => ["c++11"]
+    depends_on "camp" => ["c++11"]
+    depends_on "clhep" => ["c++11"]
+    depends_on "geant4" => ["c++11"]
+    depends_on "root5" => ["c++11"]
+    depends_on "xerces-c" => ["c++11"]
+  else
+    depends_on "boost"
+    depends_on "camp"
+    depends_on "clhep"
+    depends_on "geant4"
+    depends_on "root5"
+    depends_on "xerces-c"
+  end
+
 
   def install
     # Want to really record formula versions etc here...
