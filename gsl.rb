a20fc66fae08ecc8d233a800a2d7a92c9c8ca757
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2016-03-14 16:58:00 +0000

    gsl: Add devel block
    Provide devel block to allow testing of GSL 1.16.
    
    NO devel/head of GSL 2.X is currently allowed due to upstream
    issues.

--- gsl.rb
+++ gsl.rb
@@ -1,17 +1,23 @@
 require 'formula'
 
+# Please don't update me to the 2.x branch yet until issues discussed in
+# https://github.com/Homebrew/homebrew/issues/45812 are resolved.
+# If you want 2.x now, file a PR in homebrew/versions. Thanks!
+
 class Gsl < Formula
   homepage 'http://www.gnu.org/software/gsl/'
-  url 'http://ftpmirror.gnu.org/gsl/gsl-1.15.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/gsl/gsl-1.15.tar.gz'
-  sha1 'd914f84b39a5274b0a589d9b83a66f44cd17ca8e'
+  stable do
+    url 'http://ftpmirror.gnu.org/gsl/gsl-1.15.tar.gz'
+    mirror 'http://ftp.gnu.org/gnu/gsl/gsl-1.15.tar.gz'
+    sha1 'd914f84b39a5274b0a589d9b83a66f44cd17ca8e'
+  end
+
+  devel do
+    url 'http://ftpmirror.gnu.org/gsl/gsl-1.16.tar.gz'
+    sha256 "73bc2f51b90d2a780e6d266d43e487b3dbd78945dd0b04b14ca5980fe28d2f53"
+  end
 
   option :universal
-
-  depends_on "cmake" => :build
-  resource "GSLCMakeSupport" do
-    url "https://github.com/SuperNEMO-DBD/GSLCMakeSupport.git", :using => :git
-  end
 
   def install
     ENV.universal_binary if build.universal?
@@ -21,35 +27,9 @@ class Gsl < Formula
     system "make" # A GNU tool which doesn't support just make install! Shameful!
     system "make install"
 
-    # Install cmake support files
-    resources.each do |r|
-      r.stage do
-        gsl_support_args = std_cmake_args
-        gsl_support_args << "-Dgsl_VERSION=#{version}"
-        system "cmake", *gsl_support_args
-        system "make install"
-      end
-    end
   end
 
   test do
-    (testpath/"CMakeLists.txt").write <<-EOS.undent
-    project(testGSL C)
-    find_package(GSL REQUIRED NO_MODULE)
-    add_executable(testGSL testGSL.c)
-    target_include_directories(testGSL PRIVATE ${GSL_INCLUDE_DIRS})
-    target_link_libraries(testGSL gsl)
-    EOS
-
-    (testpath/"testGSL.c").write <<-EOS.undent
-    #include <gsl/gsl_version.h>
-    int main(int argc, char** argv) {
-    return 0;
-    }
-    EOS
-
-    system "cmake", *std_cmake_args
-    system "make"
-    system "./testGSL"
+    system bin/"gsl-randist", "0", "20", "cauchy", "30"
   end
 end
