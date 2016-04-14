4f9b8e9ecaefe477eb182fbefdd1e79995594f7d
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2016-04-14 14:58:19 +0100

    root5 5.34.36
    Update to new version to fix several important bugs on OS X and
    C++11 mode.

--- Formula/root5.rb
+++ Formula/root5.rb
@@ -2,16 +2,8 @@ class Root5 < Formula
    homepage "http://root.cern.ch"
 
    stable do
-     version "5.34.30"
-     sha1 "28fce7f2adc775f7327ee54dcca11199f605b22c"
-     url "https://root.cern.ch/download/root_v#{version}.source.tar.gz"
-     mirror "http://ftp.riken.jp/pub/ROOT/root_v#{version}.source.tar.gz"
-     patch :DATA
-   end
-
-   devel do
-     version "5.34.34"
-     sha256 "8c1faf893ed3b279f3500368b3dcd2087352020a69d3055c4d36726e7f6acd58"
+     version "5.34.36"
+     sha256 "fc868e5f4905544c3f392cc9e895ef5571a08e48682e7fe173bd44c0ba0c7dcd"
      url "https://root.cern.ch/download/root_v#{version}.source.tar.gz"
      mirror "http://ftp.riken.jp/pub/ROOT/root_v#{version}.source.tar.gz"
    end
@@ -64,105 +56,4 @@ class Root5 < Formula
      end
    end
 end
-__END__
-diff --git a/cmake/modules/RootBuildOptions.cmake b/cmake/modules/RootBuildOptions.cmake
-index 09ed495..298ebc0 100644
---- a/cmake/modules/RootBuildOptions.cmake
-+++ b/cmake/modules/RootBuildOptions.cmake
-@@ -197,21 +197,6 @@ if(DEFINED c++11)   # For backward compatibility
-   set(cxx11 ${c++11} CACHE BOOL "" FORCE)
- endif()
- 
--#---General Build options----------------------------------------------------------------------
--# use, i.e. don't skip the full RPATH for the build tree
--set(CMAKE_SKIP_BUILD_RPATH  FALSE)
--# when building, don't use the install RPATH already (but later on when installing)
--set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE) 
--# add the automatically determined parts of the RPATH
--# which point to directories outside the build tree to the install RPATH
--set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
--
--# the RPATH to be used when installing---------------------------------------------------------
--if(rpath)
--  set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
--  set(CMAKE_BUILD_WITH_INSTALL_RPATH TRUE) 
--endif()
--
- #---Avoid creating dependencies to 'non-statndard' header files -------------------------------
- include_regular_expression("^[^.]+$|[.]h$|[.]icc$|[.]hxx$|[.]hpp$")
- 
-@@ -229,6 +214,20 @@ endif()
- #---Add Installation Variables------------------------------------------------------------------
- include(RootInstallDirs)
- 
-+#---General Build options----------------------------------------------------------------------
-+# use, i.e. don't skip the full RPATH for the build tree
-+set(CMAKE_SKIP_BUILD_RPATH  FALSE)
-+# when building, don't use the install RPATH already (but later on when installing)
-+set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE) 
-+# add the automatically determined parts of the RPATH
-+# which point to directories outside the build tree to the install RPATH
-+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
-+
-+# the RPATH to be used when installing---------------------------------------------------------
-+if(rpath)
-+  set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_FULL_LIBDIR}")
-+  set(CMAKE_BUILD_WITH_INSTALL_RPATH TRUE) 
-+endif()
- 
- 
- 
-diff --git a/cmake/modules/SetUpLinux.cmake b/cmake/modules/SetUpLinux.cmake
-index 080f38a..b1d0a30 100644
---- a/cmake/modules/SetUpLinux.cmake
-+++ b/cmake/modules/SetUpLinux.cmake
-@@ -54,7 +54,7 @@ if(CMAKE_COMPILER_IS_GNUCXX)
-   set(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "${CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS}")
-   set(CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS "${CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS}")
- 
--  set(CMAKE_SHARED_LINKER_FLAGS "-Wl,--no-undefined")
-+  set(CMAKE_SHARED_LINKER_FLAGS "-Wl,--no-undefined ${CMAKE_SHARED_LINKER_FLAGS}")
- 
-   # Select flags.
-   set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g -DNDEBUG")
-@@ -89,7 +89,7 @@ elseif(CMAKE_CXX_COMPILER_ID STREQUAL Clang)
-   set(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "${CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS}")
-   set(CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS "${CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS}")
- 
--  set(CMAKE_SHARED_LINKER_FLAGS "-Wl,--no-undefined")
-+  set(CMAKE_SHARED_LINKER_FLAGS "-Wl,--no-undefined ${CMAKE_SHARED_LINKER_FLAGS}")
- 
-   # Select flags.
-   set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g -DNDEBUG")
-@@ -145,25 +145,25 @@ elseif(CMAKE_CXX_COMPILER_ID STREQUAL Intel)
-   if(ICC_MAJOR EQUAL 11)
-     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${BIT_ENVIRONMENT} -wd1572 -wd279")
-     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${BIT_ENVIRONMENT} -wd1572 -wd279")
--    set(CMAKE_SHARED_LINKER_FLAGS "${BIT_ENVIRONMENT} -Wl,--no-undefined")
-+    set(CMAKE_SHARED_LINKER_FLAGS "${BIT_ENVIRONMENT} -Wl,--no-undefined ${CMAKE_SHARED_LINKER_FLAGS}")
-   endif()
- 
-   if(ICC_MAJOR EQUAL 12)
-     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${BIT_ENVIRONMENT} -wd1572 -wd279")
-     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${BIT_ENVIRONMENT} -wd1572 -wd279")
--    set(CMAKE_SHARED_LINKER_FLAGS "${BIT_ENVIRONMENT} -Wl,--no-undefined")
-+    set(CMAKE_SHARED_LINKER_FLAGS "${BIT_ENVIRONMENT} -Wl,--no-undefined ${CMAKE_SHARED_LINKER_FLAGS}")
-   endif()
- 
-   if(ICC_MAJOR EQUAL 13)
-     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${BIT_ENVIRONMENT} -wd1572 -wd279")
-     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${BIT_ENVIRONMENT} -wd1572 -wd279")
--    set(CMAKE_SHARED_LINKER_FLAGS "${BIT_ENVIRONMENT} -Wl,--no-undefined")
-+    set(CMAKE_SHARED_LINKER_FLAGS "${BIT_ENVIRONMENT} -Wl,--no-undefined ${CMAKE_SHARED_LINKER_FLAGS}")
-   endif()
- 
-   if(ICC_MAJOR EQUAL 14)
-     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${BIT_ENVIRONMENT} -wd1572 -wd279 -wd2536 -wd873")
-     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${BIT_ENVIRONMENT} -wd1572 -wd279 -wd2536 -wd873")
--    set(CMAKE_SHARED_LINKER_FLAGS "${BIT_ENVIRONMENT} -Wl,--no-undefined")
-+    set(CMAKE_SHARED_LINKER_FLAGS "${BIT_ENVIRONMENT} -Wl,--no-undefined ${CMAKE_SHARED_LINKER_FLAGS}")
-   endif()
- 
-   set(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "${CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS}")
-
 
