03c17ec10780e59974ea4c78e856cff11643c13d
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2016-04-14 15:00:33 +0100

    Always build for C++11
    Always build Bayeux and Falaise in C++11 mode to allow upstream
    use of C++11 in plugins.

--- Formula/bayeux.rb
+++ Formula/bayeux.rb
@@ -11,33 +11,24 @@ class Bayeux < Formula
     sha256 "000ceb4313ab07a500847bcb274a93f686a7e225e1f550672d8d97f5bc3fb2b2"
   end
 
-  option :cxx11
-
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
 
   depends_on "supernemo-dbd/cadfael/gsl"
   depends_on "readline"
 
-  if build.cxx11?
-    depends_on "supernemo-dbd/cadfael/boost" => "c++11"
-    depends_on "supernemo-dbd/cadfael/camp" => "c++11"
-    depends_on "supernemo-dbd/cadfael/clhep" => "c++11"
-    depends_on "supernemo-dbd/cadfael/root5" => "c++11"
-    depends_on "supernemo-dbd/cadfael/geant4" => "c++11"
-  else
-    depends_on "supernemo-dbd/cadfael/boost"
-    depends_on "supernemo-dbd/cadfael/camp"
-    depends_on "supernemo-dbd/cadfael/clhep"
-    depends_on "supernemo-dbd/cadfael/root5"
-    depends_on "supernemo-dbd/cadfael/geant4"
-  end
+  needs :cxx11
+  depends_on "supernemo-dbd/cadfael/boost" => "c++11"
+  depends_on "supernemo-dbd/cadfael/camp" => "c++11"
+  depends_on "supernemo-dbd/cadfael/clhep" => "c++11"
+  depends_on "supernemo-dbd/cadfael/root5" => "c++11"
+  depends_on "supernemo-dbd/cadfael/geant4" => "c++11"
 
   def install
     # Micro patch to correct setting of argv with const char* instead of char
     inreplace "source/bxdatatools/src/kernel.cc", "'\\0'", "\"\\0\""
 
-    ENV.cxx11 if build.cxx11?
+    ENV.cxx11
     mkdir "bayeux.build" do
       bx_cmake_args = std_cmake_args
       bx_cmake_args << "-DCMAKE_INSTALL_LIBDIR=lib"
--- Formula/falaise.rb
+++ Formula/falaise.rb
@@ -5,19 +5,16 @@ class Falaise < Formula
   version "2.0.1"
   sha256 "01991eea64267585b4303b6e77a49b6113a0ea51815cf983d28dc73ae6ecd931"
 
-  option :cxx11
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
 
-  # Bayeux dependency will pull in all additional deps of Falaise
-  if build.cxx11?
-    depends_on "supernemo-dbd/cadfael/bayeux" => "c++11"
-  else
-    depends_on "supernemo-dbd/cadfael/bayeux"
-  end
+  needs :cxx11
+
+  # Bayeux dependency pulls in all additional deps of Falaise at present
+  depends_on "supernemo-dbd/cadfael/bayeux"
 
   def install
-    ENV.cxx11 if build.cxx11?
+    ENV.cxx11
     mkdir "falaise.build" do
       fl_cmake_args = std_cmake_args
       fl_cmake_args << "-DCMAKE_INSTALL_LIBDIR=lib"
