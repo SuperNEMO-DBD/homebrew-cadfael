d4f873720532c89348b68622173bd0ec12f4b626
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2016-07-13 16:11:43 +0100

    geant4: Add 10.2.2 as devel
    To allow tests of Bayeux against newer Geant4, supply latest
    production 10.2.2 as a devel spec.
    
    Note that this requires the devel spec of CLHEP (2.3.1.0) to also
    be installed. This must be done manually as we cannot depend on the
    devel spec...

--- Formula/geant4.rb
+++ Formula/geant4.rb
@@ -1,25 +1,33 @@
 class Geant4 < Formula
   homepage "http://geant4.cern.ch"
-  url "http://geant4.cern.ch/support/source/geant4.9.6.p04.tar.gz"
-  version "9.6.4"
-  sha256 "997220a5386a43ac8f533fc7d5a8360aa1fd6338244d17deeaa583fb3a0f39fd"
   revision 3
 
-  patch do
-    url "https://files.warwick.ac.uk/supernemo/files/Cadfael/distfiles/geant4-9.6.4-data-export.patch"
-    sha256 "6d7b50f504b53c924dfae28562726b839e191c4c78139dfa33040dfd460aebed"
+  stable do
+    url "http://geant4.cern.ch/support/source/geant4.9.6.p04.tar.gz"
+    version "9.6.4"
+    sha256 "997220a5386a43ac8f533fc7d5a8360aa1fd6338244d17deeaa583fb3a0f39fd"
+    patch do
+      url "https://files.warwick.ac.uk/supernemo/files/Cadfael/distfiles/geant4-9.6.4-data-export.patch"
+      sha256 "6d7b50f504b53c924dfae28562726b839e191c4c78139dfa33040dfd460aebed"
+    end
+    patch do
+      url "https://files.warwick.ac.uk/supernemo/files/Cadfael/distfiles/geant4-9.6.4-xcode.patch"
+      sha256 "0efa7f5b6c25f20493a3268dbd492ee3334f7839d2008554d57584ec9e4e7617"
+    end
+    patch do
+      url "https://files.warwick.ac.uk/supernemo/files/Cadfael/distfiles/geant4-9.6.4-c11.patch"
+      sha256 "c99f760125f185f436a9191c5cdbad7053e7c41aaac0f6ccbacab392787f39a9"
+    end
+    patch do
+      url "https://files.warwick.ac.uk/supernemo/files/Cadfael/distfiles/geant4-9.6.4-xercesc-include.patch"
+      sha256 "668d78b7c24efe9065a4e1aadd5441c129a454113eae96812c77a2c8861bfa64"
+    end
   end
-  patch do
-    url "https://files.warwick.ac.uk/supernemo/files/Cadfael/distfiles/geant4-9.6.4-xcode.patch"
-    sha256 "0efa7f5b6c25f20493a3268dbd492ee3334f7839d2008554d57584ec9e4e7617"
-  end
-  patch do
-    url "https://files.warwick.ac.uk/supernemo/files/Cadfael/distfiles/geant4-9.6.4-c11.patch"
-    sha256 "c99f760125f185f436a9191c5cdbad7053e7c41aaac0f6ccbacab392787f39a9"
-  end
-  patch do
-    url "https://files.warwick.ac.uk/supernemo/files/Cadfael/distfiles/geant4-9.6.4-xercesc-include.patch"
-    sha256 "668d78b7c24efe9065a4e1aadd5441c129a454113eae96812c77a2c8861bfa64"
+
+  devel do
+    url "http://geant4.cern.ch/support/source/geant4.10.02.p02.tar.gz"
+    version "10.2.2"
+    sha256 "702fb0f7a78d4bdf1e3f14508de26e4db5e2df6a21a8066a92b7e6ce21f4eb2d"
   end
 
   option :cxx11
