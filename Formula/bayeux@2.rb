e74a7318c1fcb3976781637493615b793db9aaaa
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2017-04-06 11:54:29 +0100

    bayeux@2: Bump to next RC

--- Formula/bayeux@2.rb
+++ Formula/bayeux@2.rb
@@ -2,10 +2,8 @@ class BayeuxAT2 < Formula
   desc "Bayeux Library"
   homepage ""
   version "2.2.0"
-  url "https://files.warwick.ac.uk/supernemo/files/Cadfael/distfiles/Bayeux-2.2.0-rc1.tar.bz2"
-  sha256 "c6b429131c5f5f527d3b474eb3fc05884f8c876d0d0e26d47c8b41a54de7e525"
-  
-  patch :DATA
+  url "https://files.warwick.ac.uk/supernemo/files/Cadfael/distfiles/Bayeux-2.2.0-rc2.tar.bz2"
+  sha256 "c33357b512154448743a576f66f4f19a0ca146c908c5cc727c6aebad2a9f5375"
 
   keg_only "Conflicts with newer production versions"
 
@@ -42,17 +40,4 @@ class BayeuxAT2 < Formula
     system "false"
   end
 end
-__END__
-diff --git a/source/bxmctools/src/g4/run_action.cc b/source/bxmctools/src/g4/run_action.cc
-index 5aa80d5..11d4aad 100644
---- a/source/bxmctools/src/g4/run_action.cc
-+++ b/source/bxmctools/src/g4/run_action.cc
-@@ -422,7 +422,7 @@ namespace mctools {
-       return;
-     }
- 
--    void run_action::dump(ostream & a_out) const
-+    void run_action::dump(std::ostream & a_out) const
-     {
-       a_out << "run_action::dump:" << std::endl;
-       a_out << "|-- Save data           : "  << _save_data_ << std::endl;
+
