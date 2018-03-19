739ecf0474f59b0053a30ce35655ee4df0a0f13a
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2018-03-19 14:22:25 +0000

    root6: tidy for python@2 changes

--- Formula/root6.rb
+++ Formula/root6.rb
@@ -5,92 +5,112 @@ class Root6 < Formula
   mirror "https://fossies.org/linux/misc/root_v6.12.04.source.tar.gz"
   version "6.12.04"
   sha256 "f438f2ae6e25496fa81df525935fb0bf2a403855d95c40b3e0f3a3e1e861a085"
+  revision 1
 
   head "http://root.cern.ch/git/root.git"
 
   depends_on "cmake" => :build
-  depends_on "openssl" => :recommended
-  depends_on "sqlite" => :recommended
-  depends_on "python" => :recommended
+  depends_on "libxml2" unless OS.mac? # For XML on Linux
+  depends_on "xz" # For LZMA
+  depends_on "openssl"
+  depends_on "sqlite"
+  depends_on "supernemo-dbd/cadfael/gsl"
+  depends_on "python@2"
 
-  # For LZMA
-  depends_on "xz"
-
-  # For XML on Linux
-  depends_on "libxml2" unless OS.mac?
-
-  depends_on "supernemo-dbd/cadfael/gsl" => :recommended
+  conflicts_with "root", :because => "SuperNEMO requires custom root build"
 
   needs :cxx11
-
-  def cmake_opt(opt, pkg = opt)
-    "-D#{opt}=#{(build.with? pkg) ? "ON" : "OFF"}"
-  end
 
   def install
     # Work around "error: no member named 'signbit' in the global namespace"
     ENV.delete("SDKROOT") if DevelopmentTools.clang_build_version >= 900
+    args = *std_cmake_args
+    args << "-DCMAKE_INSTALL_ELISPDIR=#{elisp}"
 
-    # brew audit doesn't like non-executables in bin
-    # so we will move {thisroot,setxrd}.{c,}sh to libexec
-    # (and change any references to them)
-    inreplace Dir["config/roots.in", "config/thisroot.*sh",
-                  "etc/proof/utils/pq2/setup-pq2",
-                  "man/man1/setup-pq2.1", "README/INSTALL", "README/README"],
-      /bin.thisroot/, "libexec/thisroot"
+    # Disable everything that might be ON by default
+    args += %w[
+      -Dalien=OFF
+      -Dasimage=OFF
+      -Dastiff=OFF
+      -Dbonjour=OFF
+      -Dcastor=OFF
+      -Dchirp=OFF
+      -Ddavix=OFF
+      -Ddcache=OFF
+      -Dfitsio=OFF
+      -Dfortran=OFF
+      -Dgfal=OFF
+      -Dglite=OFF
+      -Dgviz=OFF
+      -Dhdfs=OFF
+      -Dkrb5=OFF
+      -Dldap=OFF
+      -Dmonalisa=OFF
+      -Dmysql=OFF
+      -Dodbc=OFF
+      -Doracle=OFF
+      -Dpgsql=OFF
+      -Dpythia6=OFF
+      -Dpythia8=OFF
+      -Dqt=OFF
+      -Drfio=OFF
+      -Dsapdb=OFF
+      -Dsrp=OFF
+      -Dunuran=OFF
+    ]
+
+    # Now the core/builtin things we want
+    args += %w[
+      -Dcxx11=ON
+      -Dfail-on-missing=ON
+      -Dgnuinstall=ON
+      -Dexplicitlink=ON
+      -Drpath=ON
+      -Dsoversion=ON
+      -Dbuiltin_asimage=ON
+      -Dasimage=ON
+      -Dbuiltin_fftw3=ON
+      -Dbuiltin_freetype=ON
+      -Droofit=ON
+      -Dgdml=ON
+      -Dminuit2=ON
+      -Dbuiltin_tbb=ON
+    ]
+
+    # Options that require an external
+    args += %w[
+      -Dsqlite=ON
+      -Dssl=ON
+      -Dmathmore=ON
+      -Dxrootd=OFF
+    ]
+
+    # Python requires a bit of finessing
+    ENV.prepend_path "PATH", Formula["python@2"].opt_libexec/"bin"
+    python_executable = Utils.popen_read("which python2").strip
+    python_version = Language::Python.major_minor_version("python2")
+
+    python_prefix = Utils.popen_read("#{python_executable} -c 'import sys;print(sys.prefix)'").chomp
+    python_include = Utils.popen_read("#{python_executable} -c 'from distutils import sysconfig;print(sysconfig.get_python_inc(True))'").chomp
+    args << "-Dpython=ON"
+
+    # cmake picks up the system's python dylib, even if we have a brewed one
+    dylib = OS.mac? ? "dylib" : "so"
+    if File.exist? "#{python_prefix}/Python"
+      python_library = "#{python_prefix}/Python"
+    elsif File.exist? "#{python_prefix}/lib/libpython#{python_version}.#{dylib}"
+      python_library = "#{python_prefix}/lib/libpython#{python_version}.#{dylib}"
+    elsif File.exist? "#{python_prefix}/lib/libpython#{python_version}.a"
+      python_library = "#{python_prefix}/lib/libpython#{python_version}.a"
+    else
+      odie "No libpythonX.Y.{dylib|so,a} file found!"
+    end
+    args << "-DPYTHON_EXECUTABLE='#{python_executable}'"
+    args << "-DPYTHON_INCLUDE_DIR='#{python_include}'"
+    args << "-DPYTHON_LIBRARY='#{python_library}'"
 
     mkdir "cmake-build" do
-      system "cmake", "..",
-        # Disable everything that might be ON by default...
-        # minimal/gminimal don't allow override...
-        "-Dalien=OFF",
-        "-Dasimage=OFF",
-        "-Dastiff=OFF",
-        "-Dbonjour=OFF",
-        "-Dcastor=OFF",
-        "-Dchirp=OFF",
-        "-Ddavix=OFF",
-        "-Ddcache=OFF",
-        "-Dfitsio=OFF",
-        "-Dfortran=OFF",
-        "-Dgfal=OFF",
-        "-Dglite=OFF",
-        "-Dgviz=OFF",
-        "-Dhdfs=OFF",
-        "-Dkrb5=OFF",
-        "-Dldap=OFF",
-        "-Dmonalisa=OFF",
-        "-Dmysql=OFF",
-        "-Dodbc=OFF",
-        "-Doracle=OFF",
-        "-Dpgsql=OFF",
-        "-Dpythia6=OFF",
-        "-Dpythia8=OFF",
-        "-Dqt=OFF",
-        "-Drfio=OFF",
-        "-Dsapdb=OFF",
-        "-Dsrp=OFF",
-        # Now the stuff we want
-        "-Dfail-on-missing=ON",
-        "-Dgnuinstall=ON",
-        "-Dexplicitlink=ON",
-        "-Drpath=ON",
-        "-Dsoversion=ON",
-        "-Dbuiltin_asimage=ON",
-        "-Dasimage=ON",
-        "-Dbuiltin_fftw3=ON",
-        "-Dbuiltin_freetype=ON",
-        "-Droofit=ON",
-        "-Dgdml=ON",
-        "-Dminuit2=ON",
-        # MT is on by default, use builtin
-        "-Dbuiltin_tbb=ON",
-        cmake_opt("python"),
-        cmake_opt("ssl", "openssl"),
-        cmake_opt("sqlite", "sqlite3"),
-        cmake_opt("xrootd"),
-        cmake_opt("mathmore", "gsl"),
-        *std_cmake_args
+      system "cmake", "..", *args
 
       # Follow upstream homebrew
       # Work around superenv stripping out isysroot leading to errors with
@@ -101,13 +121,12 @@ class Root6 < Formula
       else
         system "make", "install"
       end
-    end
 
-    libexec.mkpath
-    mv Dir["#{bin}/*.*sh"], libexec
+      chmod 0755, Dir[bin/"*.*sh"]
+    end
   end
 
-  def caveats; <<-EOS.undent
+  def caveats; <<~EOS
     Because ROOT depends on several installation-dependent
     environment variables to function properly, you should
     add the following commands to your shell initialization
@@ -124,17 +143,20 @@ class Root6 < Formula
   end
 
   test do
-    (testpath/"test.C").write <<-EOS.undent
+    (testpath/"test.C").write <<~EOS
       #include <iostream>
       void test() {
         std::cout << "Hello, world!" << std::endl;
       }
     EOS
-    (testpath/"test.bash").write <<-EOS.undent
-      . #{libexec}/thisroot.sh
+    (testpath/"test.bash").write <<~EOS
+      . #{bin}/thisroot.sh
       root -l -b -n -q test.C
     EOS
     assert_equal "\nProcessing test.C...\nHello, world!\n",
-      `/bin/bash test.bash`
+                 shell_output("/bin/bash test.bash")
+
+    ENV["PYTHONPATH"] = #{lib}/"root"
+    system "python2", "-c", "'import ROOT'"
   end
 end
