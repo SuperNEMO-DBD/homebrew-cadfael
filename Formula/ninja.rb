797cec0674e24ac12df7ec9521c14faeab59b557
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2018-03-21 11:22:23 +0000

    ninja: New vendored formula
    Upsstream linuxbrew places an uneeded runtime dependence on Python.
    Vendor and use python@2 until that's fixed.

---
+++ Formula/ninja.rb
@@ -0,0 +1,29 @@
+class Ninja < Formula
+  desc "Small build system for use with gyp or CMake"
+  homepage "https://ninja-build.org/"
+  url "https://github.com/ninja-build/ninja/archive/v1.8.2.tar.gz"
+  sha256 "86b8700c3d0880c2b44c2ff67ce42774aaf8c28cbf57725cb881569288c1c6f4"
+  head "https://github.com/ninja-build/ninja.git"
+
+  depends_on "python@2" => :build unless OS.mac? 
+
+  def install
+    system "python", "configure.py", "--bootstrap"
+    bin.install "ninja"
+    bash_completion.install "misc/bash-completion" => "ninja-completion.sh"
+    zsh_completion.install "misc/zsh-completion" => "_ninja"
+  end
+
+  test do
+    (testpath/"build.ninja").write <<~EOS
+      cflags = -Wall
+
+      rule cc
+        command = gcc $cflags -c $in -o $out
+
+      build foo.o: cc foo.c
+    EOS
+    system bin/"ninja", "-t", "targets"
+  end
+end
+
