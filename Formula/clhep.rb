df4ba14d5f48c445b372db15aff758b0675af338
Xavier Garrido <xavier.garrido@gmail.com>
2019-02-07 11:35:02 +0100

    Revert automatic deletion of trailing whitespaces

--- Formula/clhep.rb
+++ Formula/clhep.rb
@@ -53,14 +53,14 @@ __END__
 --- a/CLHEP/Matrix/src/Vector.cc.orig
 +++ b/CLHEP/Matrix/src/Vector.cc
 @@ -114,9 +114,9 @@ HepVector::HepVector(const HepMatrix &hm1)
-
+ 
  // trivial methods
-
--inline int HepVector::num_row() const {return nrow;}
--inline int HepVector::num_size() const {return nrow;}
+ 
+-inline int HepVector::num_row() const {return nrow;} 
+-inline int HepVector::num_size() const {return nrow;} 
 -inline int HepVector::num_col() const { return 1; }
 +int HepVector::num_row() const {return nrow;}
 +int HepVector::num_size() const {return nrow;}
 +int HepVector::num_col() const { return 1; }
-
+ 
  // operator()
